5eacc644923c5c367230b00dac56b003fed99119
Glenn Condron <glennc@microsoft.com>
2018-09-11 21:37:51 -0700

    add

---
+++ SwaggerApp/Controllers/AdditionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AdditionController : ControllerBase
+    {
+        [HttpGet("{a}/{b}")]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
